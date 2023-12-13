050ae8250c1904869b73fe8f71876016e6cda38c
Vlad Nemirovskii <v.nemirovskii@viseven.com>
2023-12-13 19:43:36 +0200

    add assets

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-<div class="mt-10">dwadwa</div>
+  <div class="width-4/5"><div class="mt-10 my-auto bg-slate-500">dw dawdwa dadwa</div></div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
